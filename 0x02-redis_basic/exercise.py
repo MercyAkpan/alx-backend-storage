ed0386b069fcd10d6479b785f91fd370d4b90395
MercyAkpan <akpanmeme@gmail.com>
2024-08-15 12:17:25 +0100

    alx trial1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,7 +15,7 @@ def count_calls(method: Callable) -> Callable:
         '''Invokes the given method after incrementing its call counter.
         '''
         if isinstance(self._redis, redis.Redis):
-            self.redis.incr(method.qualname_)
+            self._redis.incr(method.__qualname__)
         return method(self, *args, **kwargs)
     return invoker
 
@@ -27,8 +27,8 @@ def call_history(method: Callable) -> Callable:
     def invoker(self, *args, **kwargs) -> Any:
         '''Returns the method's output after storing its inputs and output.
         '''
-        in_key = '{}:inputs'.format(method._qualname_)
-        out_key = '{}:outputs'.format(method._qualname_)
+        in_key = '{}:inputs'.format(method.__qualname__)
+        out_key = '{}:outputs'.format(method.__qualname__)
         if isinstance(self._redis, redis.Redis):
             self._redis.rpush(in_key, str(args))
         output = method(self, *args, **kwargs)
@@ -41,12 +41,12 @@ def call_history(method: Callable) -> Callable:
 def replay(fn: Callable) -> None:
     '''Displays the call history of a Cache class' method.
     '''
-    if fn is None or not hasattr(fn, '_self_'):
+    if fn is None or not hasattr(fn, '__self__'):
         return
-    redis_store = getattr(fn._self_, '_redis', None)
+    redis_store = getattr(fn.__self__, '_redis', None)
     if not isinstance(redis_store, redis.Redis):
         return
-    fxn_name = fn._qualname_
+    fxn_name = fn.__qualname__
     in_key = '{}:inputs'.format(fxn_name)
     out_key = '{}:outputs'.format(fxn_name)
     fxn_call_count = 0
@@ -66,7 +66,7 @@ def replay(fn: Callable) -> None:
 class Cache:
     '''Represents an object for storing data in a Redis data storage.
     '''
-    def _init_(self) -> None:
+    def __init__(self) -> None:
         '''Initializes a Cache instance.
         '''
         self._redis = redis.Redis()
